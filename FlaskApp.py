e10aad8180c0b2c41eb8d11d8cbc0bd5950ebce8
Senuch <s.uzairtariq@gmail.com>
2017-09-17 19:48:59 +0500

    Tutorial Video 8 !Completed!

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template
+from content_management import Content
+
+TOPIC_DICT=Content()
 
 app = Flask(__name__)
 
@@ -6,6 +9,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("main.html")
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template("dashboard.html",CONTEXT=TOPIC_DICT)
 
 
 if __name__ == '__main__':
