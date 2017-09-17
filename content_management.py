4c7d3be5c10975dddcd3e8ef50884a08a0dcddad
Senuch <s.uzairtariq@gmail.com>
2017-09-17 19:50:22 +0500

    Tutorial Video 8 Completed

---
+++ content_management.py
@@ -0,0 +1,7 @@
+def Content():
+    TOPIC_DICT={"Basics":[["Introduction to python","/introduction-to-python-programming/"],
+                          ["Print functions and strings","/python-tutorial-print-function-strings/"],
+                          ["Math basics with Python3","/python-math-basics/"]],
+                "Web Development":[[]]}
+
+    return TOPIC_DICT;
