7a669c0e5adc7a13bf69f928ceb84cd6bc28e8b8
Senuch-Uzair-Tariq <uzair.episodicx@gmail.com>
2017-10-12 00:40:49 +0500

    Video 22

--- content_management.py
+++ content_management.py
@@ -1,7 +1,40 @@
-def Content():
-    TOPIC_DICT={"Basics":[["Introduction to python","/introduction-to-python-programming/"],
-                          ["Print functions and strings","/python-tutorial-print-function-strings/"],
-                          ["Math basics with Python3","/python-math-basics/"]],
-                "Web Development":[[]]}
+##// CURRENT:
+##// list of topics by {TOPIC:[["TITLE", "URL"]]}
+##
+##// FUTURE:
+##////list of topics by {TOPIC:[["TITLE", "URL", "TAGS"],
+##////                          ["TITLE", "URL", "TAGS"]]}
 
-    return TOPIC_DICT;
+def Content():
+    ##    //             Suggest Branches for next steps
+    ##    //             If liked: Matplotlib, link to data analysis or Pandas maybe
+    ##    //             If liked: GUI stuff: Kivy, PyGame, Tkinter
+    ##    //             if liked: Text and word-based: NLTK
+
+
+    # MAIN : [TITLE, URL, BODY_TEXT (LIST), HINTS(LIST)]
+    TOPIC_DICT = {
+        "Basics": [["Python Introduction", "/introduction-to-python-programming/", [""]],
+                             ["Print Function and Strings", "/python-tutorial-print-function-strings/",
+                              ["The Print function outputs text to the console (black area). Let's try it out!",
+                               "print() is a function, which does something with parameters, and parameters go inside the parenthesis.",
+                               "See if you can use the print function to output 'Hello!' to the console!"]],
+                             ["Sockets with Python Intro", "/python-sockets/"],
+                             ["Simple Port Scanner with Sockets", "/python-port-scanner-sockets/"],
+                             ["Threaded Port Scanner", "/python-threaded-port-scanner/"],
+                             ["Binding and Listening with Sockets", "/python-binding-listening-sockets/"],
+                             ["Client Server System with Sockets", "/client-server-python-sockets/"],
+                             ["Python 2to3 for Converting Python 2 scripts to Python 3",
+                              "/converting-python2-to-python3-2to3/"]]
+                  }
+
+    return TOPIC_DICT
+
+
+if __name__ == "__main__":
+    x = Content()
+
+    print(x["Basics"])
+
+    for each in x["Basics"]:
+        print(each[1])
